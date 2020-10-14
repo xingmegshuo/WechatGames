239bca4f00972059980be43704d9abb72626547e
xingmegshuo <xms.chnb@gmail.com>
2020-10-14 15:57:50 +0800

    model change

--- mini/models.py
+++ mini/models.py
@@ -112,7 +112,7 @@ class Order(models.Model):
     date = models.DateTimeField(verbose_name=_('订单创建时间'), help_text=_('订单创建时间'), auto_now_add=True)
 
     def on_save(self):
-        self.number = self.unionId + self.pk
+        self.number = self.unionId + str(self.date)
         money = []
         virtual_money = []
         if self.product.product.is_discount is True:
