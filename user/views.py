79460eadf8eb75e0d663cc831fef263450ab7d98
xingmegshuo <xms.chnb@gmail.com>
2020-10-19 12:27:53 +0800

    remove login_time

--- user/views.py
+++ user/views.py
@@ -124,8 +124,7 @@ def record_time(user):
     user.last_login = user.login
     user.login = timezone.now()
     user.save()
-    record_login = RecordLogin.objects.create(user=user)
-    record_login.save()
+
 
 
 class WxLoginView(APIView):
