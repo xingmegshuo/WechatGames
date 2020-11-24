5a870e6aac3879fb1f82b5559ac928244cc4d114
xingmegshuo <xms.chnb@gmail.com>
2020-11-24 17:46:21 +0800

    update server

--- mini/views.py
+++ mini/views.py
@@ -274,7 +274,7 @@ class OrderApi(APIView):
         import time
         timestamp = str(int(time.time()))
         import requests
-        response = request.post('https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi', bodyData.encode('utf-8'),
+        response = requests.post('https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi', bodyData.encode('utf-8'),
                                 headers={'Content': 'application/xml'})
         import xmltodict
         content = xmltodict.parse(response.content)
