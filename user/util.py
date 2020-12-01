e4e3b18a3461c7fe6bea5ab297e6b7bf4928fe66
xingmegshuo <xms.chnb@gmail.com>
2020-12-01 16:59:10 +0800

    model change

--- user/util.py
+++ user/util.py
@@ -13,7 +13,6 @@ def deal_ip(request, id):
         try:
             user = Userip.objects.get(ip=ip)
             user.count += 1
-            user.name = id
             user.save()
         except:
             response = reader.city(ip)
