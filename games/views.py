687ebd70c581d1926d74bcd7f6cbacce6d92fdfb
xms <xms.chnb@gmail.com>
2022-03-16 22:25:47 +0800

    :smile:update

--- games/views.py
+++ games/views.py
@@ -1096,10 +1096,10 @@ class InviterNewView(APIView):
                                          'city', 'province', 'country', 'login', 'openid', ]) for i in frends]
         inviterby = {}
 
-        # if len(inby) > 0:
-            # inviterby = model_to_dict(MyUser.objects.get(id=inby[0].inviter_id), fields=['nick_name', 'last_login', 'avatar_url', 'gender',
-                                                    #    'city', 'province', 'country', 'login', 'openid', ])
-        return Response({'status': 1, 'mes': "inviter,我的邀请,inviterby,我的邀请人", 'data': {'inviter': inviter, 'inviterby': inviterby,'len':len(inby)}}, HTTP_200_OK)
+        if len(inby) > 0:
+            inviterby = model_to_dict(inby[0].inviter_id, fields=['nick_name', 'last_login', 'avatar_url', 'gender',
+                                                       'city', 'province', 'country', 'login', 'openid', ])
+        return Response({'status': 1, 'mes': "inviter,我的邀请,inviterby,我的邀请人", 'data': {'inviter': inviter, 'inviterby': inviterby}}, HTTP_200_OK)
 
     def post(self, request):
         param = get_parameter_dic(request)
